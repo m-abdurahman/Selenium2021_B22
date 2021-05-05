727153cd7736276507c0df7e116e1a34638b23fe
gurhank <gurhankocyigit@gmail.com>
2021-05-05 18:31:22 -0500

    BasicNavigations class

--- src/test/java/com/cybertek/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cybertek/tests/day1_selenium_intro/BasicNavigations.java
@@ -4,19 +4,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTest {
+public class BasicNavigations {
 
     public static void main(String[] args) {
 
-        //1- Setting up the browser driver
+        //1- setup the browser driver
         WebDriverManager.chromedriver().setup();
 
-        //2- Create instance of chrome driver and open browser
-        //This is the line that is opening the browser.
+        //2- create instance of selenium web driver
+        // this line opens a browser
         WebDriver driver = new ChromeDriver();
 
-        //3- Go to https://www.google.com
-        driver.get("https://www.google.com");
+        //3- get the page for Tesla.com
+        driver.get("https://www.tesla.com");
+
 
     }
 
